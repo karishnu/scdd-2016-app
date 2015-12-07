d8e0565f760acfe111c9520216658ad4b3851f76
Amartya-Duke <amartya.2k9@gmail.com>
2015-12-07 17:42:58 +0530

    RowElements for event list view. From now on use this package for all row elements of lists

---
+++ app/src/main/java/com/startupsclub/scdd/RowElements/CEvents.java
@@ -0,0 +1,39 @@
+package com.startupsclub.scdd.RowElements;
+
+/**
+ * Created by Amartya on 12/7/2015.
+ */
+public class CEvents {
+
+    String year;
+    String date;
+    String day;
+    String title;
+    String venue;
+
+   public  CEvents(String year,String date,String day,String title,String venue) {
+        this.year = year;
+        this.date =date;
+        this.day=day;
+        this.title=title;
+        this.venue=venue;
+    }
+    public String get_year(){
+        return year;
+    }
+    public String get_date(){
+        return date;
+    }
+
+    public String get_day(){
+        return day;
+    }
+    public String get_title(){
+        return title;
+    }
+    public String get_venue(){
+        return venue;
+    }
+
+
+}
